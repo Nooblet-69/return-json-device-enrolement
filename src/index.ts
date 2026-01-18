d9ab0a8821d2244a0fb82e47f2de2dde88f3c972
Nooblet-69 <53385134+Nooblet-69@users.noreply.github.com>
2026-01-18 20:21:43 +1100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 export default {
 	async fetch(request): Promise<Response> {
-		const data = {
-			hello: "world",
-		};
+		const data = {"Servers":[{"Version":"mdm-adde","BaseURL":"https:\/\/biz-40460.business.mosyle.io\/api\/v2enrollment\/enroll\/adde?model-family=Mac"}]};
 
 		return Response.json(data);
 	},
